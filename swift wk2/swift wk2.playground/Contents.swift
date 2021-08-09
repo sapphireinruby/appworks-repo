f93aebd86fa23736c5dca7c380e80ec9eb638cd1
Amber Liu <sapphireinruby@gmail.com>
2021-08-09 21:05:24 +0800

    Catch the error! Complete the assignment!

--- swift wk2/swift wk2.playground/Contents.swift
+++ swift wk2/swift wk2.playground/Contents.swift
@@ -371,31 +371,53 @@ let steven = Person(name: "Steven", toolMan: ())
 
 //Error Handling in Swift
 
- 
+
+//enum GuessNumberGameError: Error {
+//    case wrongNumber
+//}
+//
+//class GuessNumerGame {
+//    var targetNumber = 10
+//
+//    func guess(number: Int) throws {
+//
+//        guard number == targetNumber else {
+//            throw GuessNumberGameError.wrongNumber
+//        }
+//        do {
+//            try guess(number: 20)
+//        } catch GuessNumberGameError.wrongNumber {
+//            print("Sorry, it's not the right number.")
+//        }
+//        print("Guess the right number: \(targetNumber)")
+//    }
+//
+//}
+  
 enum GuessNumberGameError: Error {
     case wrongNumber
 }
+
 class GuessNumerGame {
     var targetNumber = 10
-    
+
     func guess(number: Int) throws {
-        
+
         guard number == targetNumber else {
             throw GuessNumberGameError.wrongNumber
         }
-        do {
-            try guess(number: 20)
-        } catch GuessNumberGameError.wrongNumber {
-            print("Sorry, it's not the right number.")
-        }
+
         print("Guess the right number: \(targetNumber)")
     }
-    
 
 }
-  
-let x = GuessNumerGame()
 
+do {
+    let whatsThisGame = GuessNumerGame()
+    try whatsThisGame.guess(number: 20)
+} catch {
+    print("Sorry, it's not the right number.")
+}
 
 
 
