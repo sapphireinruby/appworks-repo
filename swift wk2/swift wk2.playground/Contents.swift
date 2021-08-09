103e9cc67870ffd5437e1d94d66aadd92da335a1
Amber Liu <sapphireinruby@gmail.com>
2021-08-09 21:28:55 +0800

    Clean up some notes

--- swift wk2/swift wk2.playground/Contents.swift
+++ swift wk2/swift wk2.playground/Contents.swift
@@ -370,29 +370,6 @@ let steven = Person(name: "Steven", toolMan: ())
 
 
 //Error Handling in Swift
-
-
-//enum GuessNumberGameError: Error {
-//    case wrongNumber
-//}
-//
-//class GuessNumerGame {
-//    var targetNumber = 10
-//
-//    func guess(number: Int) throws {
-//
-//        guard number == targetNumber else {
-//            throw GuessNumberGameError.wrongNumber
-//        }
-//        do {
-//            try guess(number: 20)
-//        } catch GuessNumberGameError.wrongNumber {
-//            print("Sorry, it's not the right number.")
-//        }
-//        print("Guess the right number: \(targetNumber)")
-//    }
-//
-//}
   
 enum GuessNumberGameError: Error {
     case wrongNumber
@@ -418,7 +395,3 @@ do {
 } catch {
     print("Sorry, it's not the right number.")
 }
-
-
-
-//Read the code above first and paste it in the playground file, there is an error inside the code. Please solve the error by adding additional code in the file. Do not remove or modify the code above. Call guess(number:) and pass 20 as the argument after you fix the problem.
