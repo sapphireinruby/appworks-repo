bf371f2fe589afbcad77d50259254dea987ac8f6
Amber Liu <sapphireinruby@gmail.com>
2021-08-12 15:37:52 +0800

    Set up the SimpleSingUpApp with title and segment

---
+++ swift wk3/SimpleLoginSignUpApp/SimpleLoginSignUpApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleLoginSignUpApp
+//
+//  Created by Amber on 8/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
