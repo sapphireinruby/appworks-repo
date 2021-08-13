d4b7a9ad87bcd20065371258c4d8a83cdf568a69
Amber Liu <sapphireinruby@gmail.com>
2021-08-13 20:52:22 +0800

    Add IBOutlets

--- swift wk3/SimpleLoginSignUpApp/SimpleLoginSignUpApp/ViewController.swift
+++ swift wk3/SimpleLoginSignUpApp/SimpleLoginSignUpApp/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var accountTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var checkTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
