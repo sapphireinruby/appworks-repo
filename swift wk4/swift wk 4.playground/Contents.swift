33211a6a950d1af471a2c090db29609e2a82353d
Amber Liu <sapphireinruby@gmail.com>
2021-08-16 22:31:29 +0800

    called function to compare

--- swift wk4/swift wk 4.playground/Contents.swift
+++ swift wk4/swift wk 4.playground/Contents.swift
@@ -7,12 +7,14 @@ func ifOdd(number: Int) -> Bool {
     return false
 }
 
+ifOdd(number: 9)
+
+
 var ifOddFunc: ((Int) -> Bool) = {number in
     if number % 2 == 1 {
         return true
     }
     return false
 }
-
 
 ifOddFunc(9)
